c787c011911913ff736848ed9bc18b4d9de94f35
binas <matib95@gmail.com>
2023-10-31 23:33:03 +0100

    Add implementation for buttons

---
+++ Zajecia2/SimpleButtonsApp/SimpleButtonsApp/MainWindowViewModel.cs
@@ -0,0 +1,53 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SimpleButtonsApp
+{
+    public partial class MainWindowViewModel : ObservableObject
+    {
+        private CancellationTokenSource _cancellationTokenSource;
+
+        private TimeSpan _timeout = TimeSpan.FromSeconds(10);
+
+        [ObservableProperty]
+        private string _infoLabel;
+
+        public MainWindowViewModel()
+        {
+        }
+
+        [RelayCommand]
+        async Task Start()
+        {
+            _cancellationTokenSource = new CancellationTokenSource();
+            InfoLabel = "Start button was clicked";
+            var token = _cancellationTokenSource.Token;
+            try
+            {
+                await StartDelayAsync(token);
+            }
+            catch (OperationCanceledException ex)
+            {
+                InfoLabel = "Process was cancelled";
+            }
+            finally 
+            {
+                _cancellationTokenSource.Dispose(); 
+            }
+        }
+
+        [RelayCommand]
+        async Task Stop(CancellationToken cancellationToken)
+        {
+            _cancellationTokenSource.Cancel();
+        }
+
+        private async Task StartDelayAsync(CancellationToken cancellationToken)
+        {
+            await Task.Delay(_timeout, cancellationToken);
+        }
+    }
+}
