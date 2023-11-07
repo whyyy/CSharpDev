8bf5efd79d1b799f6c3563411e4c65b3fa285ff5
binas <matib95@gmail.com>
2023-11-07 17:13:11 +0100

    Add own async method

--- Zajecia2/SimpleButtonsAppMultipleDelays/SimpleButtonsAppMultipleDelays/MainWindowViewModel.cs
+++ Zajecia2/SimpleButtonsAppMultipleDelays/SimpleButtonsAppMultipleDelays/MainWindowViewModel.cs
@@ -2,8 +2,6 @@
 {
     using CommunityToolkit.Mvvm.ComponentModel;
     using CommunityToolkit.Mvvm.Input;
-    using System;
-    using System.Collections.Generic;
     using System.Threading;
     using System.Threading.Tasks;
 
@@ -35,32 +33,29 @@
 
         private async Task StartMultipleDelaysAsync(CancellationToken cancellationToken)
         {
-            var longDelayTask = Task.Delay(TimeSpan.FromSeconds(15), cancellationToken);
-            var shortDelayTask = Task.Delay(TimeSpan.FromSeconds(5), cancellationToken);
-            var mediumDelayTask = Task.Delay(TimeSpan.FromSeconds(10), cancellationToken);
-
-            var delayTasks = new List<Task> { shortDelayTask, mediumDelayTask, longDelayTask };
-
-            delayTasks.ForEach(t => OutputPrinter.Print($"Id: {t.Id}, status: {t.Status}, time: {DateTime.Now}"));
-
             try
             {
-                while (delayTasks.Count > 0)
+                while (await IsInfiniteLoopRunningAsync(cancellationToken))
                 {
-                    var delayFinishedTask = await Task.WhenAny(delayTasks);
-
-                    OutputPrinter.Print($"Finished task, id: {delayFinishedTask.Id}, "
-                                        + $"status: {delayFinishedTask.Status}, "
-                                        + $"time: {DateTime.Now}");
-                    delayTasks.Remove(delayFinishedTask);
                 }
+                
             }
             catch (TaskCanceledException ex)
             {
                 InfoLabel = $"Process was cancelled";
-                OutputPrinter.Print("Remaining tasks:");
-                delayTasks.ForEach(t => OutputPrinter.Print($"Id: {t.Id}, status: {t.Status}, time: {DateTime.Now}"));
             }
+        }
+
+        private async Task<bool> IsInfiniteLoopRunningAsync(CancellationToken cancellationToken)
+        {
+            return await Task.Run(() =>
+            {
+                while (!cancellationToken.IsCancellationRequested)
+                {
+                    return true;
+                }
+                throw new TaskCanceledException();
+            });
         }
     }
 }
