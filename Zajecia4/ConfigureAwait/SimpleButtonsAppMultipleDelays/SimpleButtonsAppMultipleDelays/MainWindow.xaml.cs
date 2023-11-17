034dcf1afa741561aed3bf2ebedfdc6cf9ce01ee
whyyy <matib95@gmail.com>
2023-11-17 17:08:35 +0100

    Configure await and Dispatcher (#5)
    * Copy existing app into new directory
    
    * Update code
    
    * Add implementation for ConfigureAwait and Dispatcher
    
    * Remove tests written for viewmodel class
    
    * Cleanup and add cancellationtoken argument
    
    * Add implementation to update frontend on ui thread
    
    ---------
    
    Co-authored-by: bwkm <bwkm@viessmann.com>

---
+++ Zajecia4/ConfigureAwait/SimpleButtonsAppMultipleDelays/SimpleButtonsAppMultipleDelays/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿namespace SimpleButtonsApp
+{
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using System.Windows;
+
+    public partial class MainWindow : Window
+    {
+        private CancellationTokenSource cancellationTokenSource;
+        public MainWindow()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void StartDrawing(object sender, RoutedEventArgs e)
+        {
+            this.cancellationTokenSource = new CancellationTokenSource();
+            await this.StartDrawingLuckyNumber(this.cancellationTokenSource.Token);
+
+        }
+
+        private void StopDrawing(object sender, RoutedEventArgs e)
+        {
+            this.cancellationTokenSource?.Cancel();
+            this.InfoLabel.Content = "Drawing finished";
+        }
+
+        private async Task<bool> StartDrawingLuckyNumber(CancellationToken cancellationToken)
+        {
+            var random = new Random();
+            int luckyNumber = 0;
+
+            await Task.Run(() =>
+            {
+                try
+                {
+                    while (!cancellationToken.IsCancellationRequested)
+                    {
+                        //this.Dispatcher.Invoke(() =>
+                        //{
+                        luckyNumber = random.Next(1, 99);
+                        Task.Delay(100, cancellationToken);
+                        //});
+                    }
+
+                    cancellationToken.ThrowIfCancellationRequested();
+                }
+                catch (OperationCanceledException ex)
+                {
+
+                }
+                catch (Exception ex)
+                {
+
+                }
+                
+            }, cancellationToken);
+
+            this.InfoLabel.Content = "Drawing is in progress";
+            this.LoopLabel.Content = $"Lucky number is: {luckyNumber}";
+
+            return false;
+        }
+    }
+}
