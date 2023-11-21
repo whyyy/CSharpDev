90778e5b1cfb1d84fce1c01495ad1e84ff836df3
bwkm <bwkm@viessmann.com>
2023-11-21 19:09:41 +0100

    Implement own control which inherits from Button

--- Zajecia6/OwnControl/DrawingLuckyNumberApp/ResultsModule/ViewModels/ResultsViewModel.cs
+++ Zajecia6/OwnControl/DrawingLuckyNumberApp/ResultsModule/ViewModels/ResultsViewModel.cs
@@ -4,15 +4,17 @@ using DrawingLuckyNumber.Core;
 using Prism.Events;
 using Prism.Mvvm;
 
-internal class ResultsViewModel : BindableBase
+public class ResultsViewModel : BindableBase
 {
     private string drawingStatus;
     private string luckyNumber;
+    private string drawingTotalTimeInSeconds;
 
     public ResultsViewModel(IEventAggregator eventAggregator)
     {
         eventAggregator.GetEvent<IsDrawingInProgressEvent>().Subscribe(this.DrawingStatusReceived);
         eventAggregator.GetEvent<LuckyNumberDrawnEvent>().Subscribe(this.LuckyNumberReceived);
+        eventAggregator.GetEvent<DrawingTotalTimeInSecondsEvent>().Subscribe(this.DrawingTotalTimeInSecondsReceived);
     }
 
     public string DrawingStatus
@@ -25,6 +27,12 @@ internal class ResultsViewModel : BindableBase
     {
         get => this.luckyNumber;
         set => SetProperty(ref this.luckyNumber, value);
+    }
+
+    public string DrawingTotalTimeInSeconds
+    {
+        get => this.drawingTotalTimeInSeconds;
+        set => SetProperty(ref this.drawingTotalTimeInSeconds, value);
     }
 
     private void DrawingStatusReceived(bool receivedDrawingStatus)
@@ -41,5 +49,16 @@ internal class ResultsViewModel : BindableBase
         }
 
         this.LuckyNumber = $"Lucky number is: {receivedLuckyNumber}";
+    }
+
+    private void DrawingTotalTimeInSecondsReceived(double totalTimeInSeconds)
+    {
+        if (totalTimeInSeconds == 0)
+        {
+            this.DrawingTotalTimeInSeconds = "";
+            return;
+        }
+
+        this.DrawingTotalTimeInSeconds = $"Drawing total time: {totalTimeInSeconds} seconds";
     }
 }
