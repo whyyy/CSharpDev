a9c968e3739c59a17005b4d0bc2ec07a1d95b3a8
whyyy <matib95@gmail.com>
2023-11-25 16:00:09 +0100

    Copy files to new directory (#9)
    Co-authored-by: bwkm <bwkm@viessmann.com>

---
+++ Zajecia7/WpfTopics/DrawingLuckyNumberApp/DrawingLuckyNumberModule/Controls/ColorfulDateTimeButton.cs
@@ -0,0 +1,40 @@
+﻿namespace DrawingLuckyNumberModule.Controls;
+
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+public class ColorfulDateTimeButton : Button
+{
+    private static readonly DependencyProperty ClickDateTimeValueProperty = DependencyProperty.Register(
+        nameof(ClickDateTimeValue), 
+        typeof(DateTime), 
+        typeof(ColorfulDateTimeButton));
+
+    private readonly Random random = new Random();
+
+    public ColorfulDateTimeButton()
+    {
+        this.Background = GetRandomBrush();
+    }
+
+    public DateTime ClickDateTimeValue
+    {
+        get => (DateTime)GetValue(ClickDateTimeValueProperty);
+        set => SetValue(ClickDateTimeValueProperty, value);
+    }
+
+    protected override void OnClick()
+    {
+        this.ClickDateTimeValue = DateTime.Now;
+        this.Background = GetRandomBrush();
+        base.OnClick();
+    }
+
+    private SolidColorBrush GetRandomBrush()
+    {
+        return new SolidColorBrush(Color.FromRgb((byte)this.random.Next(1, 255),
+                                                 (byte)this.random.Next(1, 255),
+                                                 (byte)this.random.Next(1, 255)));
+    }
+}
