766775af977001a0ab75f41b5c8f85a3ced1784b
bwkm <bwkm@viessmann.com>
2023-12-05 18:48:49 +0100

    Add Validation Rule

---
+++ Zajecia9/WpfTopics/DrawingLuckyNumberApp/DrawingLuckyNumberApp.UI/Controls/ValidationRules/RangeValueValidationRule.cs
@@ -0,0 +1,26 @@
+﻿namespace DrawingLuckyNumberApp.UI.Controls.ValidationRules
+{
+    using System.Globalization;
+    using System.Windows.Controls;
+
+    public class RangeValueValidationRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value,
+                                                  CultureInfo cultureInfo)
+        {
+            try
+            {
+                if (value.ToString().Length > 0)
+                {
+                    Int32.Parse(value.ToString());
+                }
+            }
+            catch (Exception ex)
+            {
+                return new ValidationResult(false, $"Unable to parse: '{value}' to int");
+            }
+
+            return ValidationResult.ValidResult;
+        }
+    }
+}
